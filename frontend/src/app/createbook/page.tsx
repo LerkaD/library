2836e85c8b55145b3ee36100c1139d4eb7e5e62c
0436645933@my.lerna.by <lerkadziatsel@gmail.com>
2025-07-26 22:51:39 +0300

    check_1

--- frontend/src/app/createbook/page.tsx
+++ frontend/src/app/createbook/page.tsx
@@ -121,7 +121,7 @@ export default function AddBookPage() {
         publisher_id: selectedPublisherId === '' ? null : selectedPublisherId,
         authors_ids: selectedAuthorIds,
       });
-      setSuccessMessage('Book sucsessfully created! check eslint');
+      setSuccessMessage('Book sucsessfully created! check ');
       setTitle('');
       setSelectedPublisherId('');
       setSelectedAuthorIds([]);
